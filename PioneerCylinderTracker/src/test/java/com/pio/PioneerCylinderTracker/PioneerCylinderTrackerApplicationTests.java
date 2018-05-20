822606bb1a74850ee0c3d0c03008bc1a12777f42
nizam <nizam@192.168.0.7>
2018-05-20 13:46:25 +0530

    First Commit

---
+++ PioneerCylinderTracker/src/test/java/com/pio/PioneerCylinderTracker/PioneerCylinderTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pio.PioneerCylinderTracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PioneerCylinderTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
